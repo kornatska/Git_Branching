9462151a511ffcd195a10eac06da81a7d872ae85
Anastasiia <anastasiia.kornatska@gmail.com>
2022-04-10 20:59:50 -0700

    Added negative testing for tc 001

--- src/test_cases/TC001.java
+++ src/test_cases/TC001.java
@@ -3,9 +3,11 @@ package test_cases;
 public class TC001 {
     public static void main(String[] args) {
 
-        //testing log in
+        // testing log in
 
         // more testing
+
+        // negative testing
 
     }
 }
